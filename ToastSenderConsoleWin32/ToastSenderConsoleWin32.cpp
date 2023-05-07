c5e85cd9d68052d1204d690f563b9e35496af21e
nobushi95 <nobushi1121@gmail.com>
2023-05-07 16:18:33 +0900

    アクティベータを追加

--- ToastSenderConsoleWin32/ToastSenderConsoleWin32.cpp
+++ ToastSenderConsoleWin32/ToastSenderConsoleWin32.cpp
@@ -8,6 +8,24 @@ using namespace ABI::Windows::Data::Xml::Dom;
 using namespace ABI::Windows::UI::Notifications;
 using namespace Microsoft::WRL;
 
+// The UUID CLSID must be unique to your app. Create a new GUID if copying this code.
+class DECLSPEC_UUID("a8a47616-b67f-4fd1-8a96-bdc29c84b7e5") NotificationActivator WrlSealed WrlFinal
+    : public RuntimeClass<RuntimeClassFlags<ClassicCom>, INotificationActivationCallback>
+{
+public:
+    virtual HRESULT STDMETHODCALLTYPE Activate(
+        _In_ LPCWSTR appUserModelId,
+        _In_ LPCWSTR invokedArgs,
+        _In_reads_(dataCount) const NOTIFICATION_USER_INPUT_DATA * data,
+        ULONG dataCount) override
+    {
+        // TODO: Handle activation
+    }
+};
+
+// Flag class as COM creatable
+CoCreatableClass(NotificationActivator);
+
 int main()
 {
     std::cout << "Hello World!\n";
