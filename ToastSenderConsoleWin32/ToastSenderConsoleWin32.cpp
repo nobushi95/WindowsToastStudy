6a5badaf4762a131f4be15f32fce333dece51181
nobushi95 <nobushi1121@gmail.com>
2023-05-07 16:15:56 +0900

    ヘッダーファイルと名前空間をインクルードする

--- ToastSenderConsoleWin32/ToastSenderConsoleWin32.cpp
+++ ToastSenderConsoleWin32/ToastSenderConsoleWin32.cpp
@@ -1,5 +1,13 @@
 ﻿#include <iostream>
 
+#include "DesktopNotificationManagerCompat.h"
+#include <NotificationActivationCallback.h>
+#include <windows.ui.notifications.h>
+
+using namespace ABI::Windows::Data::Xml::Dom;
+using namespace ABI::Windows::UI::Notifications;
+using namespace Microsoft::WRL;
+
 int main()
 {
     std::cout << "Hello World!\n";
